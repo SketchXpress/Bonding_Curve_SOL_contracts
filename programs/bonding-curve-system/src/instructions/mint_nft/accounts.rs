93d267f2eb492a932458a4f96887b1a89af05043
Ang Sherpa <141511150+nodeClimber@users.noreply.github.com>
2025-08-17 08:16:28 -0400

    refactor: Remove unused bump and escrow implementations for NFT minting

--- programs/bonding-curve-system/src/instructions/mint_nft/accounts.rs
+++ programs/bonding-curve-system/src/instructions/mint_nft/accounts.rs
@@ -10,17 +10,8 @@ pub struct MintNftArgs {
     pub collection_mint: Pubkey,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize)]
-pub struct MintNftAccounts {
-    pub pool_bump: u8,
-    pub metadata_bump: u8,
-    pub minter_tracker_bump: u8,
-    pub collection_metadata_bump: u8,
-}
-
 #[derive(Accounts)]
 #[instruction(args: MintNftArgs)]
-#[derive(Debug)]
 pub struct MintNft<'info> {
     #[account(mut)]
     pub minter: Signer<'info>,
@@ -30,7 +21,7 @@ pub struct MintNft<'info> {
         payer = minter,
         mint::decimals = 0,
         mint::authority = minter,
-        mint::freeze_authority = minter,
+        mint::freeze_authority = minter
     )]
     pub nft_mint: Account<'info, Mint>,
 
@@ -38,7 +29,7 @@ pub struct MintNft<'info> {
         init,
         payer = minter,
         associated_token::mint = nft_mint,
-        associated_token::authority = minter,
+        associated_token::authority = minter
     )]
     pub minter_token_account: Account<'info, TokenAccount>,
 
@@ -47,19 +38,14 @@ pub struct MintNft<'info> {
     #[account(
         mut,
         seeds = [b"bonding-curve-pool", collection_mint.key().as_ref()],
-        bump,
+        bump
     )]
     pub pool: Account<'info, crate::state::BondingCurvePool>,
 
     #[account(
         mut,
-        seeds = [
-            b"metadata",
-            token_metadata_program.key().as_ref(),
-            nft_mint.key().as_ref(),
-        ],
-        bump,
-        seeds::program = token_metadata_program.key()
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), nft_mint.key().as_ref()],
+        bump
     )]
     /// CHECK: Created by token metadata program
     pub metadata: UncheckedAccount<'info>,
@@ -74,23 +60,21 @@ pub struct MintNft<'info> {
     pub minter_tracker: Account<'info, MinterTracker>,
 
     #[account(
-        seeds = [
-            b"metadata",
-            token_metadata_program.key().as_ref(),
-            collection_mint.key().as_ref(),
-        ],
-        bump,
-        seeds::program = token_metadata_program.key()
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump
     )]
     /// CHECK: Verified by collection mint constraint
     pub collection_metadata: UncheckedAccount<'info>,
 
     #[account(address = anchor_spl::token::ID)]
     pub token_program: Program<'info, Token>,
+
     pub associated_token_program: Program<'info, anchor_spl::associated_token::AssociatedToken>,
+
     #[account(address = mpl_token_metadata::ID)]
     /// CHECK: Verified by address constraint
     pub token_metadata_program: UncheckedAccount<'info>,
+
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 }
--- programs/bonding-curve-system/src/instructions/mint_nft/bumps.rs
+++
@@ -1,7 +0,0 @@
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
-pub struct MintNftBumps {
-    pub pool: u8,
-    pub metadata: u8,
-    pub minter_tracker: u8,
-    pub collection_metadata: u8,
-}
